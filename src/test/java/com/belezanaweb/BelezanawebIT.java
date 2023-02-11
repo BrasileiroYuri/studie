770e6b2debf6135aa4c19b7886bcd2d89458318c
Yuri Santos <yurimax322@gmail.com>
2023-02-10 21:13:33 -0300

    Adicionando teste de API.

--- src/test/java/com/belezanaweb/BelezanawebIT.java
+++ src/test/java/com/belezanaweb/BelezanawebIT.java
@@ -1,5 +1,9 @@
 package com.belezanaweb;
 
+import com.belezanaweb.util.ResourceUtils;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,36 +12,44 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
-import com.belezanaweb.api.controller.ProductController;
-import com.belezanaweb.util.ResourceUtils;
-
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BelezanawebIT {
 
-	@LocalServerPort
-	private int port;
+    @LocalServerPort
+    private int port;
 
-	@Autowired
-	ProductController productController;
-	@Autowired
-	ResourceUtils resourceUtils;
+    @Autowired
+    private Flyway flyway;
 
-	private String payloadJson;
+    private String correctJson;
+    private String jsonWithId;
 
-	@BeforeEach
-	public void setUp() {
-		RestAssured.basePath = "/products";
-		RestAssured.port = port;
-		payloadJson = resourceUtils.getContentFromResource("/json/payloadJson.json");
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-	}
+    @BeforeEach
+    public void setUp() {
+        RestAssured.basePath = "/products";
+        RestAssured.port = port;
+        getJsons();
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        flyway.migrate();
+    }
 
-	@Test
-	public void deveRetornarStatus201Created_quandoAdicionarProduto() {
-		RestAssured.given().contentType(ContentType.JSON).accept(ContentType.JSON).body(payloadJson).when().post()
-				.then().statusCode(HttpStatus.CREATED.value()).log().all();
-	}
+    private void getJsons() {
+        correctJson = ResourceUtils.getContentFromResource("/json/correctBody.json");
+        jsonWithId = ResourceUtils.getContentFromResource("/json/jsonWithCorrectId.json");
+    }
+
+    @Test
+    public void shouldFail_WhenRegisterExistingProduct() {
+        given().contentType(ContentType.JSON).accept(ContentType.JSON).body(jsonWithId).when()
+                .post().then().statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    public void shouldReturnStatus201Created_WhenToAddProduct() {
+        given().contentType(ContentType.JSON).accept(ContentType.JSON).body(correctJson).when()
+                .post().then().statusCode(HttpStatus.CREATED.value()).log().all();
+    }
+
 }
